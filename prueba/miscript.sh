4f60b25df58f6d9a59c8d42c4265c47bd61680eb
Guadalupe Lopez <lvg051091@gmail.com>
2022-09-21 21:45:07 -0500

    reestructuracion de proyecto

---
+++ prueba/miscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#todas las lineas que comiencen con gatito  son comentarios
+
+echo
+echo
+touch archivo.txt
+mkdir eduardo
+mkdir karla
+mkdir alex
+touch eduardo/perfil.txt
+touch karla/perfil2.txt
+
+ echo
+whoami 
+
